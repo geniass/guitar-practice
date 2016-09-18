eef411d98628f3e0d49d1a9d87008fa25d1266d3
Ari Croock <ari@synthesis.co.za>
2016-09-18 18:49:56 +0200

    generate all chords pairs when a new chord is added

--- minute_changes/models.py
+++ minute_changes/models.py
@@ -17,17 +17,40 @@ class Chord(models.Model):
     def __str__(self):
         return self.name
 
+class ChordPair(models.Model):
+    chord1 = models.ForeignKey(Chord, related_name='chord1', on_delete=models.PROTECT)
+    chord2 = models.ForeignKey(Chord, related_name='chord2', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "{} <-> {}".format(self.chord1, self.chord2)
+
 class MinuteChanges(models.Model):
     duration = models.IntegerField(default=1)
     date = models.DateTimeField('Practice Time')
-    chord1 = models.ForeignKey(Chord, related_name='chord1', on_delete=models.PROTECT)
-    chord2 = models.ForeignKey(Chord, related_name='chord2', on_delete=models.PROTECT)
+    chord_pair = models.ForeignKey(ChordPair, on_delete=models.PROTECT)
     changes = models.IntegerField(default=0)
 
     def __str__(self):
-        return "{}: ({} to {}) {} change(s) in {} minute(s)".format(
+        return "{}: ({}) {} change(s) in {} minute(s)".format(
                 str(self.date),
-                self.chord1,
-                self.chord2,
+                self.chord_pair,
                 self.changes,
                 self.duration)
+
+def create_chord_pairs(instance, created, raw, **kwargs):
+    # not a new chord
+    if not created or raw:
+        return
+
+    # get all current chords excluding the new one
+    chords = Chord.objects.exclude(name=instance.name)
+    if len(chords) < 1:
+        # no existing chords, can't make pairs
+        return
+
+    for chord in chords:
+        # create the new pairs. Order doesn't matter
+        ChordPair.objects.create(chord1=instance, chord2=chord)
+
+
+models.signals.post_save.connect(create_chord_pairs, sender=Chord, dispatch_uid='create_chord_pairs')
--- minute_changes/views.py
+++ minute_changes/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
 from .models import MinuteChanges
 
 def index(req):
     recent_minute_changes = MinuteChanges.objects.order_by('-date')[:5]
-    output = '\n'.join(recent_minute_changes)
-    return HttpResponse(output)
+    context = {
+        'recent_minute_changes': recent_minute_changes,
+    }
+    return render(req, 'minute_changes/index.html', context)
 
 def detail(req, minute_changes_id):
-    return HttpResponse('changes: %s' % minute_changes_id)
+    change = get_object_or_404(MinuteChanges, pk=minute_changes_id)
+    return render(req, 'minute_changes/detail.html', {'change': change})
